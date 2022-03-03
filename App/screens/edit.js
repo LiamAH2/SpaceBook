9ee17f351035b23f9600f2de17abbb1131bcb1e2
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-03 13:37:46 +0000

    Update edit.js
    Adding more update features

--- App/screens/edit.js
+++ App/screens/edit.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Button, StyleSheet, Image, } from 'react-native';
+import {View, Text, Button, StyleSheet, Image, ScrollView, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
@@ -25,6 +25,9 @@ class EditScreen extends Component {
     this.unsubscribe();
   }
   
+
+  
+  //getting user profile picture
   getLogo = async () =>
   {
     const userId = await AsyncStorage.getItem('@user_id');
@@ -48,7 +51,7 @@ class EditScreen extends Component {
             console.log(error);
         })
   }
-
+  //getting user data
   getUserData = async () => 
   {
     const userId = await AsyncStorage.getItem('@user_id');
@@ -63,6 +66,8 @@ class EditScreen extends Component {
                 return response.json()
             }else if(response.status === 401){
               console.log("Un Aurthorised");
+            }else if(response.status === 403){
+                console.log("Forbidden");
             }else if(response.status === 404){
               console.log("Not Found");
             }else if(response.status === 500){
@@ -108,7 +113,7 @@ class EditScreen extends Component {
             console.log(error);
         })
   }
-
+//checking user logged in
   checkLoggedIn = async () => {
     const value = await AsyncStorage.getItem('@session_token');
     if (value == null) {
@@ -116,6 +121,43 @@ class EditScreen extends Component {
     }
   };
 
+//edditing current user info
+  edit = async () => {
+    const userId = await AsyncStorage.getItem('@user_id');
+    const value = await AsyncStorage.getItem('@session_token');
+    let to_send = {};
+
+    if (this.state.first_name != this.state.first_name){
+      to_send['user_givenname'] = this.state.first_name;
+    }
+
+    if (this.state.last_name != this.state.last_name){
+      to_send['user_familyname'] = this.state.last_name;
+    }
+
+    if (this.state.email != this.state.email){
+      to_send['user_email'] = this.state.email;
+    }
+
+    console.log(JSON.stringify(to_send));
+
+    return fetch("http://localhost:3333/api/1.0.0/user/", {
+        method: 'PATCH',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(to_send)
+    })
+    .then((responseJson) => {
+        this.setState({
+            listData: responseJson
+          })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+//displaying to page
   render() {
     
     if (this.state.isLoading){
@@ -147,15 +189,34 @@ class EditScreen extends Component {
 
           <Text>{this.state.userData.email}</Text>
 
-          <Button
-          title="Return to profile"
-          onPress={() => this.props.navigation.navigate("Home")}
-          />
-          <Button
-          title="Save Changes"
-          onPress={() => this.props.navigation.navigate("Edit")}
-          />
-
+        <ScrollView>
+                <TextInput
+                    placeholder="Enter new first name"
+                    onChangeText={(first_name) => this.setState({first_name})}
+                    value={this.state.first_name}
+                    style={{padding:5, borderWidth:1, margin:5}}
+                />
+                <TextInput
+                    placeholder="enter new last name"
+                    onChangeText={(last_name) => this.setState({last_name})}
+                    value={this.state.last_name}
+                    style={{padding:5, borderWidth:1, margin:5}}
+                />
+                <TextInput
+                    placeholder="Enter new email"
+                    onChangeText={(email) => this.setState({email})}
+                    value={this.state.email}
+                    style={{padding:5, borderWidth:1, margin:5}}
+                />
+                <Button
+                    title="Update info"
+                    onPress={() => this.edit()}
+                />
+                <Button
+                    title="Return home"
+                    onPress={() => this.props.navigation.navigate("Home")}
+                />
+            </ScrollView>
         </View>
       );
     }
@@ -178,6 +239,10 @@ const styles = StyleSheet.create(
     {
       width: 200,
       height: 200
+    },
+    butttongroup:
+    {
+      flex:1  
     }
   });
 
