b639bc2f8e133298e077dc8a797287458b853118
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-08 12:47:41 +0000

    new file uploads

---
+++ App/screens/editstack.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Edit from './edit';
+import UpdatePhoto from './updatephoto';
+
+const Stack = createNativeStackNavigator();
+
+function EditStack() {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Edit" component={Edit} />
+        <Stack.Screen name="UpdatePhoto" component={UpdatePhoto} />
+      </Stack.Navigator>
+  );
+}
+
+export default EditStack;
