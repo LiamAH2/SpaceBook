f85f149be2327b70410d20dc35e5b235cd0ac542
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-11 23:46:18 +0000

    Order organisation
    reordering drawer links

--- App/App.js
+++ App/App.js
@@ -8,8 +8,8 @@ import LoginScreen from './screens/login';
 import SignupScreen from './screens/signup';
 import LogoutScreen from './screens/logout';
 import EditStack from './screens/editstack';
-//import UpdateDetails from './screens/updatedetails';
 import FriendsScreen from './screens/friends';
+import PostScreen from './screens/posts';
 const Drawer = createDrawerNavigator();
 
 class App extends Component{
@@ -19,11 +19,11 @@ class App extends Component{
                 <Drawer.Navigator initialRouteName="Login">
                     <Drawer.Screen name="Home" component={HomeScreen} />
                     <Drawer.Screen name="Login" component={LoginScreen} />
+                    <Drawer.Screen name="Friends" component={FriendsScreen} />
+                    <Drawer.Screen name="Posts" component={PostScreen} />
+                    <Drawer.Screen name="Edit" component={EditStack} />
                     <Drawer.Screen name="Signup" component={SignupScreen} />
                     <Drawer.Screen name="Logout" component={LogoutScreen} />
-                    <Drawer.Screen name="Edit" component={EditStack} />
-                    
-                    <Drawer.Screen name="Friends" component={FriendsScreen} />
                 </Drawer.Navigator>
                 
             </NavigationContainer>
