56e88c214c8c45205291057b6cc2edf4745f1efa
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-11 17:49:31 +0000

    Created friends page
    Created friends page to list requests, send requests and display friends within the page. displaying current friends not currently working

--- App/screens/friends.js
+++ App/screens/friends.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Button, StyleSheet, Image, FlatList } from 'react-native';
+import {View, Text, Button, StyleSheet,FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class FriendsScreen extends Component {
@@ -9,16 +9,21 @@ class FriendsScreen extends Component {
       this.state = {
         isLoading: true,
         listData: [],
-        userData: []
+        userData: [],
+        requestList: [],
+        friendList: []
       }
     }
 
     componentDidMount() {
         this.unsubscribe = this.props.navigation.addListener('focus', () => {
           this.checkLoggedIn();
+          this.getData();
+          this.requestList();
         });
       
         this.getData();
+        this.requestList();
       }
     
       componentWillUnmount() {
@@ -59,38 +64,219 @@ class FriendsScreen extends Component {
         }
       };
     
+
+      //search friend quety GET
+      listFriend = async () => {
+        const value = await AsyncStorage.getItem('@session_token');
+        const userId = await AsyncStorage.getItem('@user_id');
+        return fetch("http://localhost:3333/api/1.0.0/user/" + userId + "/friends", {
+          method: 'GET',
+              'headers': {
+                'X-Authorization':  value,
+                'Content-Type': 'application/json'
+              }
+            })
+            .then((response) => {
+                if(response.status === 200){
+                    return response.json()
+                }else if(response.status === 401){
+                  this.props.navigation.navigate("Login");
+                }else{
+                    throw 'Something went wrong';
+                }
+            })
+            .then((responseJson) => {
+              this.setState({
+                isLoading: false,
+                friendList: responseJson
+              })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+      }
+        //Add new friend POST
+        /*
+        addFriend = async() => {
+          const userId = await AsyncStorage.getItem('@user_id');
+          const value = await AsyncStorage.getItem('@session_token');
+  
+          return fetch("http://localhost:3333/api/1.0.0/user" + userId, {
+              method: 'post',
+              headers: {
+                'X-Authorization':  value,
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(this.state)
+          })
+          .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 401){
+              console.log("Un Aurthorised");
+            }else if(response.status === 403){
+                console.log("Forbidden");
+            }else if(response.status === 404){
+              console.log("Not Found");
+            }else if(response.status === 500){
+              console.log("Server Error");
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+          .then((responseJson) => {
+                 console.log("friend added", responseJson);
+                 this.props.navigation.navigate("Home");
+          })
+          .catch((error) => {
+              console.log(error);
+          })
+      }
+*/
+      //LEAVE EVERYTHING ALONE BELOW HERE LIAM
+        //GET requests (display list)
+        requestList = async () => {
+          const value = await AsyncStorage.getItem('@session_token');
+          return fetch("http://localhost:3333/api/1.0.0/friendrequests", {
+            method: 'GET',
+                'headers': {
+                  'Content-Type': 'application/json',
+                  'X-Authorization':  value
+                }
+              })
+              .then((response) => {
+                if(response.status === 200){
+                    return response.json()
+                }else if(response.status === 401){
+                  console.log("Un Aurthorised");
+                }else if(response.status === 500){
+                  console.log("Server Error");
+                }else{
+                    throw 'Something went wrong';
+                }
+            })
+              .then((responseJson) => {
+                this.setState({
+                  isLoading: false,
+                  requestList: responseJson
+                })
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+        }
+        //Accept request POST
+        acceptRequest = async (user_id) => {
+          const value = await AsyncStorage.getItem('@session_token');
+          return fetch("http://localhost:3333/api/1.0.0/friendrequests/" + user_id, {
+            method: 'POST',
+                'headers': {
+                  'X-Authorization':  value
+                }
+              })
+              .then((response) => {
+                  if(response.status === 200){
+                      return response.json()
+                  }else if(response.status === 401){
+                    this.props.navigation.navigate("Login");
+                  }else{
+                      throw 'Something went wrong';
+                  }
+              })
+              .then((responseJson) => {
+                this.setState({
+                  isLoading: false,
+                  listData: responseJson
+                })
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+        }
+        //reject request DELETE
+        rejectRequest = async (user_id) => {
+          const value = await AsyncStorage.getItem('@session_token');
+          return fetch("http://localhost:3333/api/1.0.0/friendrequests/" + user_id, {
+            method: 'DELETE',
+                'headers': {
+                  'X-Authorization':  value
+                }
+              })
+              .then((response) => {
+                  if(response.status === 200){
+                      return response.json()
+                  }else if(response.status === 401){
+                    this.props.navigation.navigate("Login");
+                  }else{
+                      throw 'Something went wrong';
+                  }
+              })
+              .then((responseJson) => {
+                this.setState({
+                  isLoading: false,
+                  listData: responseJson
+                  
+                })
+              })
+              .catch((error) => {
+                  console.log(error);
+              })
+        }
+        
       render() {
-    
-        if (this.state.isLoading){
           return (
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Text>Loading..</Text>
-            </View>
-          );
-        }else{
-          return (
-            <View>
-              <FlatList
-                    data={this.state.listData}
+            <View style={styles.container1}>
+                  <FlatList
+                    data={this.state.requestList}
                     renderItem={({item}) => (
                         <View>
-                          <Text>{item.user_givenname} {item.user_familyname}</Text>
+                          
+                          <Text>{item.first_name} {item.last_name}</Text>
+                          <Button
+                    title="Accept"
+                    onPress={() => this.acceptRequest(item.user_id)}
+                />
+                <Button
+                    title="Deny"
+                    onPress={() => this.rejectRequest(item.user_id)}
+                />
                         </View>
+                    )}
+                    keyExtractor={(item,index) => item.user_id.toString()}
+                  />
+                  <FlatList
+                    data={this.state.friendList}
+                    renderItem={({item}) => (
+                        <View>
+                          
+                          <Text>{item.user_givenname} {item.user_familyname}</Text>
+                          
+                        </View>   
                     )}
                     keyExtractor={(item,index) => item.user_id.toString()}
                   />
             </View>
           );
         }
-        
       }
 
-}
+
 
 export default FriendsScreen;
+
+const styles = StyleSheet.create(
+  {
+    container1:
+    {
+      flex:1,
+      flexDirection: 'column',
+      justifyContent:'space-evenly',
+      alignItems: 'center',
+      padding: 10
+    },
+    logo:
+    {
+      width: 200,
+      height: 200
+    }
+  });
