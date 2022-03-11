a32c1ea02a05be2a031db60f2caf7dbf8b502d21
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-11 23:48:07 +0000

    Update home.js
    adding status codes and removing unused code

--- App/screens/home.js
+++ App/screens/home.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import {View, Text, Button, StyleSheet, Image, } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet, Image, } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 class HomeScreen extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -17,108 +17,94 @@ class HomeScreen extends Component {
   componentDidMount() {
     this.unsubscribe = this.props.navigation.addListener('focus', () => {
       this.checkLoggedIn();
+      this.getUserData();
+      this.getPicture();
     });
     this.getUserData();
+    this.getPicture();
   }
 
   componentWillUnmount() {
     this.unsubscribe();
   }
-  
-  getLogo = async () =>
-  {
-    const userId = await AsyncStorage.getItem('@user_id');
-    const value = await AsyncStorage.getItem('@session_token');
-    return fetch("http://localhost:3333/api/1.0.0/user/" + userId + "/photo", {
-          'headers': {
-            'X-Authorization':  value
-          }
-        })
-        .then((res) => {
-            return res.blob();
-        })
-        .then((resBlob) => {
-          let data = URL.createObjectURL(resBlob);
-          this.setState({
-            isLoading: false,
-            photo: data
-          })
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-  }
 
-  getUserData = async () => 
-  {
-    const userId = await AsyncStorage.getItem('@user_id');
-    const value = await AsyncStorage.getItem('@session_token');
-    return fetch("http://localhost:3333/api/1.0.0/user/" + userId, {
-          'headers': {
-            'X-Authorization':  value
-          }
-        })
-        .then((response) => {
-            if(response.status === 200){
-                return response.json()
-            }else if(response.status === 401){
-              console.log("Un Aurthorised");
-            }else if(response.status === 404){
-              console.log("Not Found");
-            }else if(response.status === 500){
-              console.log("Server Error");
-            }else{
-                throw 'Something went wrong';
-            }
-        })
-        .then((responseJson) => {
-          this.setState({
-            userData: responseJson
-          })
-          this.getData();
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-  }
-
-  getData = async () => {
-    const value = await AsyncStorage.getItem('@session_token');
-    return fetch("http://localhost:3333/api/1.0.0/search", {
-          'headers': {
-            'X-Authorization':  value
-          }
-        })
-        .then((response) => {
-            if(response.status === 200){
-                return response.json()
-            }else if(response.status === 401){
-              this.props.navigation.navigate("Login");
-            }else{
-                throw 'Something went wrong';
-            }
-        })
-        .then((responseJson) => {
-          this.setState({
-            listData: responseJson
-          })
-          this.getLogo();
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-  }
-
+  //checking user is logged in, if not, redirect to log in page
   checkLoggedIn = async () => {
     const value = await AsyncStorage.getItem('@session_token');
     if (value == null) {
-        this.props.navigation.navigate('Login');
+      this.props.navigation.navigate('Login');
     }
   };
 
+  //getting logged in users profile picture
+  getPicture = async () => {
+    const userId = await AsyncStorage.getItem('@user_id');
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/user/" + userId + "/photo", {
+      'headers': {
+        'X-Authorization': value,
+        'Content-Type': 'image/png'
+      }
+    })
+      .then((response) => {
+        if(response.status === 200){
+          return response.blob();
+        } else if (response.status === 401) {
+          console.log("Un Aurthorised");
+        } else if (response.status === 404) {
+          console.log("Not Found");
+        } else if (response.status === 500) {
+          console.log("Server Error");
+        } else {
+          throw 'Something went wrong';
+        } 
+      })
+      .then((responsePicture) => {
+        let data = URL.createObjectURL(responsePicture);
+        this.setState({
+          isLoading: false,
+          photo: data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+//getting user data from the database to be displayed
+  getUserData = async () => {
+    const userId = await AsyncStorage.getItem('@user_id');
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/user/" + userId, {
+      'headers': {
+        'X-Authorization': value,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json()
+        } else if (response.status === 401) {
+          console.log("Un Aurthorised");
+        } else if (response.status === 404) {
+          console.log("Not Found");
+        } else if (response.status === 500) {
+          console.log("Server Error");
+        } else {
+          throw 'Something went wrong';
+        }
+      })
+      .then((responseJson) => {
+        this.setState({
+          userData: responseJson
+        })
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+//rending to the screen
   render() {
-    
-    if (this.state.isLoading){
+
+    if (this.state.isLoading) {
       return (
         <View
           style={{
@@ -130,47 +116,54 @@ class HomeScreen extends Component {
           <Text>Loading..</Text>
         </View>
       );
-    }else{
+    } else {
+      //displaying user image, first and last name as well as email
+      //styling being called from stylesheet
+      //buttons for navigating specific screens
       return (
-          <View style={styles.container1}>
+        <View style={styles.container1}>
 
           <Image
-          style={styles.logo}
-          source={{uri: this.state.photo}}
+            style={styles.logo}
+            source={{ uri: this.state.photo}}
           />
-          
+
           <Text>{this.state.userData.first_name}</Text>
           <Text>{this.state.userData.last_name}</Text>
 
           <Text>{this.state.userData.email}</Text>
 
           <Button
-          title="Log Out"
-          onPress={() => this.props.navigation.navigate("Logout")}
+            title="Edit Profile"
+            onPress={() => this.props.navigation.navigate("Edit")}
           />
           <Button
-          title="Edit Profile"
-          onPress={() => this.props.navigation.navigate("Edit")}
+            title="Friends"
+            onPress={() => this.props.navigation.navigate("Friends")}
           />
           <Button
-          title="Friends"
-          onPress={() => this.props.navigation.navigate("Friends")}
+            title="Posts"
+            onPress={() => this.props.navigation.navigate("Posts")}
           />
-
+          <Button
+            title="Log Out"
+            onPress={() => this.props.navigation.navigate("Logout")}
+          />
         </View>
       );
     }
-    
+
   }
 }
-
+export default HomeScreen;
+//style sheet for page styling
 const styles = StyleSheet.create(
   {
     container1:
     {
-      flex:1,
+      flex: 1,
       flexDirection: 'column',
-      justifyContent:'space-evenly',
+      justifyContent: 'space-evenly',
       alignItems: 'center',
       padding: 10
     },
@@ -182,4 +175,4 @@ const styles = StyleSheet.create(
   });
 
 
-export default HomeScreen;
+
