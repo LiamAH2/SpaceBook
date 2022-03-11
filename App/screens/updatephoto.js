df462de17686fd386bf4fdd1b52cc197939a3527
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-11 23:48:53 +0000

    Update updatephoto.js
    Adding comments to camera stack

--- App/screens/updatephoto.js
+++ App/screens/updatephoto.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Camera } from 'expo-camera';
 
+//updating users picture using the camera
 class UpdatePhoto extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -13,112 +14,116 @@ class UpdatePhoto extends Component {
       type: Camera.Constants.Type.back
     }
   }
-
+//checking if permissions have been granted or denied for the camera
   async componentDidMount() {
     this.unsubscribe = this.props.navigation.addListener('focus', async () => {
-      this.checkLoggedIn();  
+      this.checkLoggedIn();
       const { status } = await Camera.requestCameraPermissionsAsync();
       console.log("Status", status);
-      this.setState({hasPermission: status === 'granted', isLoading: false});
+      this.setState({ hasPermission: status === 'granted', isLoading: false });
     });
     const { status } = await Camera.requestCameraPermissionsAsync();
     console.log("Status", status);
-    this.setState({hasPermission: status === 'granted', isLoading: false});
-    
+    this.setState({ hasPermission: status === 'granted', isLoading: false });
+
   }
 
   componentWillUnmount() {
     this.unsubscribe();
-    
+
   }
 
- 
-//checking user logged in
+
+  //checking user logged in
   checkLoggedIn = async () => {
     const value = await AsyncStorage.getItem('@session_token');
     if (value == null) {
-        this.props.navigation.navigate('Login');
+      this.props.navigation.navigate('Login');
     }
   };
 
- 
 
+//sending new photo to database using POST method
   sendToServer = async (data) => {
     // Get these from AsyncStorage
     const userId = await AsyncStorage.getItem('@user_id');
     const value = await AsyncStorage.getItem('@session_token');
 
-      let res = await fetch(data.base64);
-      let blob = await res.blob();
+    let res = await fetch(data.base64);
+    let blob = await res.blob();
 
-      return fetch("http://localhost:3333/api/1.0.0/user/" + userId + "/photo", {
-          method: "POST",
-          headers: {
-              "Content-Type": "image/png",
-              "X-Authorization": value
-          },
-          body: blob
-      })
+    //sending photo data to DB using POST
+    return fetch("http://localhost:3333/api/1.0.0/user/" + userId + "/photo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/png",
+        "X-Authorization": value
+      },
+      body: blob
+    })
+    //navigation to gome page when picture has been taken
       .then((response) => {
-          console.log("Picture added", response);
-          this.props.navigation.navigate("Home");
+        console.log("Picture added", response);
+        this.props.navigation.navigate("Home");
       })
       .catch((err) => {
-          console.log(err);
+        console.log(err);
       })
-}
+  }
+//taking picture of the user and calling the sendToServer function above
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {
+        quality: 0.5,
+        base64: true,
+        onPictureSaved: (data) => this.sendToServer(data)
+      };
+      //setting camera options using above options
+      await this.camera.takePictureAsync(options);
+    }
+  }
 
-takePicture = async () => {
-    if(this.camera){
-        const options = {
-            quality: 0.5, 
-            base64: true,
-            onPictureSaved: (data) => this.sendToServer(data)
-        };
-        await this.camera.takePictureAsync(options); 
-    } 
-}
 
-
-//displaying to page
+  //displaying to page
   render() {
-    console.log("Permission", this.state.hasPermission)
-    if(this.state.hasPermission){
-        return(
-          <View style={styles.container}>
-            <Camera 
-              style={styles.camera} 
-              type={this.state.type}
-              ref={ref => this.camera = ref}
-            >
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                  style={styles.button}
-                  onPress={() => {
-                    this.takePicture();
-                  }}>
-                  <Text style={styles.text}> Take Photo </Text>
-                </TouchableOpacity>
-              </View>
-            </Camera>
-          </View>
-        );
-      }else{
-        return(
-          <Text>No access to camera</Text>
-        );
-      }
-    }  
+    //if camera has permissions granted, will display it to page
+    if (this.state.hasPermission) {
+      return (
+        <View style={styles.container}>
+          <Camera
+            style={styles.camera}
+            type={this.state.type}
+            ref={ref => this.camera = ref}
+          >
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  this.takePicture();
+                }}>
+                <Text style={styles.text}> Take Photo </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    } else {
+      //no permission shows below message
+      return (
+        <Text>No access to camera</Text>
+      );
+    }
+  }
 }
 
-
+//styling for the camera
 const styles = StyleSheet.create(
   {
     container:
     {
-      flex:1,
+      flex: 1,
       flexDirection: 'column',
-      justifyContent:'space-evenly',
+      justifyContent: 'space-evenly',
       alignItems: 'center',
       padding: 10
     },
