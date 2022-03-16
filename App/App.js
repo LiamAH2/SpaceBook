5ff4009b23ddf8eea1ac21cc4a2d9fe99135f1fc
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-16 16:22:58 +0000

    Update App.js
    Added css import

--- App/App.js
+++ App/App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import './App.css';
 
 import HomeScreen from './screens/home';
 import LoginScreen from './screens/login';
