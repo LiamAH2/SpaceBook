03097e0a5d75cd85216e95aa02882cb5b858b977
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-18 17:35:38 +0000

    Update App.js
    Changed some of the navigation to stack navigation

--- App/App.js
+++ App/App.js
@@ -2,37 +2,36 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/Stack';
-import './App.css';
 
 import HomeScreen from './screens/home';
 import LoginScreen from './screens/login';
 import SignupScreen from './screens/signup';
 import LogoutScreen from './screens/logout';
 
-//Stacks
+// Stacks
 import EditStack from './screens/editstack';
 import FriendStack from './screens/friendstack';
 import PostStack from './screens/poststack';
 
 const Stack = createStackNavigator();
 
-class App extends Component{
-    render(){
-        return (
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Login">
-                    <Stack.Screen name="Home" component={HomeScreen} />
-                    <Stack.Screen name="Login" component={LoginScreen} />
-                    <Stack.Screen name="Posts" component={PostStack} />
-                    <Stack.Screen name="Edit" component={EditStack} />
-                    <Stack.Screen name='Friends' component={FriendStack} />
-                    <Stack.Screen name="Signup" component={SignupScreen} />
-                    <Stack.Screen name="Logout" component={LogoutScreen} />
-                </Stack.Navigator>
-                
-            </NavigationContainer>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Posts" component={PostStack} />
+          <Stack.Screen name="Edit" component={EditStack} />
+          <Stack.Screen name="Friends" component={FriendStack} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Logout" component={LogoutScreen} />
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    );
+  }
 }
 
 export default App;
