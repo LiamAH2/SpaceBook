b7ddb4d5ffaf6b7a17231a75ddf70b0a6532e268
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-18 17:35:13 +0000

    Update .eslintrc.js
    Added extra rules to eslint file

--- App/.eslintrc.js
+++ App/.eslintrc.js
@@ -24,5 +24,12 @@ module.exports = {
     'react/jsx-filename-extension': [0],
     'no-else-return': 0,
     'consistent-return': 0,
+    'react/no-unused-state': 0,
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 0,
+    'no-use-before-define': 0,
+    'no-return-assign': 0,
+    'react/prefer-stateless-function': 0,
+    'import/no-unresolved': 0,
   },
 };
