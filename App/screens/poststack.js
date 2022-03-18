71695867b7792775322f88a1db37602bef83c352
LiamAH2 <99747106+LiamAH2@users.noreply.github.com>
2022-03-18 01:38:55 +0000

    eslint

---
+++ App/screens/poststack.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Posts from './posts';
+
+const Stack = createNativeStackNavigator();
+
+function FriendStack() {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Posts" component={Posts} />
+      </Stack.Navigator>
+  );
+}
+
+export default FriendStack;
